703f8a418121956c134da0dec5201a588b43bc47
daniel <168971595+cruzboj@users.noreply.github.com>
2025-12-26 18:44:28 +0200

    test

--- src/main.py
+++ src/main.py
@@ -1,16 +1,11 @@
-import os
+# import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = os.getenv("CORS_ALLOWED_ORIGINS", "").split(",")
-
-print(f"Allowed CORS origins: {origins}")
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
