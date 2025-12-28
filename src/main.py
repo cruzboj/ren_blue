6ce969ff9f04466c9044002e0ec09e697dd44853
daniel <168971595+cruzboj@users.noreply.github.com>
2025-12-29 00:12:50 +0200

    allow origin

--- src/main.py
+++ src/main.py
@@ -1,12 +1,19 @@
 import os
+from dotenv import load_dotenv, find_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+load_dotenv(find_dotenv())
+
 app = FastAPI()
+
+raw_origins = os.getenv("CORS_ALLOWED_ORIGINS", "NOT_FOUND_IN_ENV")
+
+allowed_origins = [origin.strip() for origin in raw_origins.split(",") if origin.strip()]
 
 app.add_middleware(
     CORSMiddleware,
-    # allow_origins=os.getenv("CORS_ALLOWED_ORIGINS", "").split(","), 
+    allow_origins=allowed_origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -18,4 +25,9 @@ app.include_router(api.router)
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {
+        "status": "Check complete",
+        # "looking_for_file": find_dotenv(),
+        # "raw_value": raw_origins,
+        # "list": allowed_origins
+    }
